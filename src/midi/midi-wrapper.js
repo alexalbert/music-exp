a85a24964fd492ddad2b92816584c1b078f94ed0
alexalbert <albalex@gmail.com>
2016-05-04 10:41:41 -0400

    refactor: Move MIDI into feature

--- src/midi/midi-wrapper.js
+++ src/midi/midi-wrapper.js
@@ -1,5 +1,8 @@
 import {inject} from 'aurelia-framework';
 import {EventAggregator} from 'aurelia-event-aggregator';
+import {NoteInfo} from '../messages';
+import {Container} from 'aurelia-dependency-injection';
+
 
 @inject(EventAggregator)
 export class MidiWrapper {
@@ -10,15 +13,24 @@ export class MidiWrapper {
   }
 
   subscribe() {
-     this.eventAggregator.subscribe('playNotes', notes => {
-       var delay = 0; // play one note every quarter second
-       var velocity = 127; // how hard the note hits
-       // play the note
-       MIDI.setVolume(0, 127);
-       for (let note of notes) {
-         MIDI.noteOn(0, note, velocity, delay);
-         MIDI.noteOff(0, note, delay + 0.75);
+     this.eventAggregator.subscribe(NoteInfo, notes => {
+       if (notes.actions.includes('play')) {
+         this.playNotes(notes);
        }
      });
    }
+
+   playNotes(notes) {
+     var delay = 0; // play one note every quarter second
+     var velocity = 127; // how hard the note hits
+     // play the note
+     MIDI.setVolume(0, 127);
+     for (let note of notes.notes) {
+       MIDI.noteOn(0, note.number, velocity, delay);
+       MIDI.noteOff(0, note.number, delay + 0.75);
+      }
+   }
 }
+
+let container = Container.instance;
+var mw = container.get(MidiWrapper);
