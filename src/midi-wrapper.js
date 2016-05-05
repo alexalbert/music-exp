cb6eaf35834676567d0a30ecde10ae14dc12c9a1
alexalbert <albalex@gmail.com>
2016-05-05 14:01:14 -0400

    refactor: move midi next to src

--- src/midi-wrapper.js
+++ src/midi-wrapper.js
@@ -1,6 +1,7 @@
 import {inject} from 'aurelia-framework';
 import {EventAggregator} from 'aurelia-event-aggregator';
-import {NoteInfo} from './messages';
+import  {NoteInfo, Action} from 'note-info';
+import {Container} from 'aurelia-dependency-injection';
 
 @inject(EventAggregator)
 export class MidiWrapper {
@@ -12,20 +13,36 @@ export class MidiWrapper {
 
   subscribe() {
      this.eventAggregator.subscribe(NoteInfo, notes => {
-       if (notes.actions.includes('play')) {
+       if (notes.actions.includes(Action.playChord)) {
+         this.playChord(notes);
+       }
+       if (notes.actions.includes(Action.play)) {
          this.playNotes(notes);
        }
      });
    }
 
-   playNotes(notes) {
+   playChord(notes) {
      var delay = 0; // play one note every quarter second
      var velocity = 127; // how hard the note hits
-     // play the note
      MIDI.setVolume(0, 127);
-     for (let note of notes.notes) {
-       MIDI.noteOn(0, note.number, velocity, delay);
-       MIDI.noteOff(0, note.number, delay + 0.75);
+     let numbers = notes.notes.map(n => { return n.number; })
+     MIDI.chordOn(0, numbers, velocity, delay);
+     MIDI.chordOff(0, numbers, delay + 0.75);
+   }
+
+   playNotes(notes) {
+     var delay = 0;
+     var velocity = 127;
+     MIDI.setVolume(0, 127);
+     for (var i = 0; i < notes.notes.length; i++) {
+       let note = notes.notes[i];
+       MIDI.noteOn(0, note.number, velocity, note.start ? note.start : delay);
+       MIDI.noteOff(0, note.number, note.end ? note.end : delay + 0.75);
       }
    }
 }
+
+// Load MIDI
+let container = Container.instance;
+var mw = container.get(MidiWrapper);
