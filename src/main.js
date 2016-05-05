e207133ef5bd18012c4bc1fdf683af95ce2f4bd8
alexalbert <albalex@gmail.com>
2016-05-05 14:10:27 -0400

    refactor: remove feature midi

--- src/main.js
+++ src/main.js
@@ -3,8 +3,7 @@ import 'bootstrap';
 export function configure(aurelia) {
   aurelia.use
     .standardConfiguration()
-    .developmentLogging()
-    .feature('midi');
+    .developmentLogging();
 
   //Uncomment the line below to enable animation.
   //aurelia.use.plugin('aurelia-animator-css');
